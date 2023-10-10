eae48045ed2d21120ba04172cd92437b0d09a5a8
ochsner3k <40705326+ochsner3k@users.noreply.github.com>
2023-10-10 14:55:28 -0400

    Create prj2.py

---
+++ prj2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read in training CSV - already merged values from exoTest.csv 
+d = pd.read_csv('./exoTrain.csv')
+
+# print("data:", d.info())
+# No null values! :D
+
+#This function plots the flux of 5 random rows over time
+def fiveRRows():
+    return 0
+
+fiveRRows()
